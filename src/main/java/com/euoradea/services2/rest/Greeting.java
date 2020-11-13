cf3936088f79017e4c4bd076c3300418546dbf9a
Fulop Alex-Dominic <fulopalex2000@gmail.com>
2020-11-13 12:15:10 +0200

    Completed Service 3

---
+++ src/main/java/com/euoradea/services2/rest/Greeting.java
@@ -0,0 +1,27 @@
+package com.euoradea.services2.rest;
+
+public class Greeting {
+    public Long id;
+    public String content;
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
