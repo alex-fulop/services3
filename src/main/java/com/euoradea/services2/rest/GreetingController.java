28190f640bf6faa713703de1d12929cb2b27d827
Fulop Alex-Dominic <fulopalex2000@gmail.com>
2020-11-13 12:51:24 +0200

    Small Refactor

--- src/main/java/com/euoradea/services2/rest/GreetingController.java
+++ src/main/java/com/euoradea/services2/rest/GreetingController.java
@@ -15,6 +15,8 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World")String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+        long id = counter.incrementAndGet();
+        String message = String.format(TEMPLATE, name);
+        return new Greeting(id, message);
     }
 }
